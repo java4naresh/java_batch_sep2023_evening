6c9c147990253f2cbe4ff3f211f5f7cb6746fba6
srinivas <srinivas.madala43@gmail.com>
2023-09-13 19:34:22 +0530

    practice1

---
+++ Srinivas/Char.java
@@ -0,0 +1,7 @@
+class Srinivas{
+public static void main (String [] args){
+String name = "Amulya";
+System.out.println(name);
+}
+}
+
---
+++ Srinivas/Number.java
@@ -0,0 +1,6 @@
+class Srinivas{
+public static void main (String [] args){
+System.out.println('1994');
+}
+}
+
