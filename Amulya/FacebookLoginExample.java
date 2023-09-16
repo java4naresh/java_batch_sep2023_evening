45b546f0491b6087c64d58d913a5d5b651dc64c6
srinivasm <srinivas.madala43@gmail.com>
2023-09-16 23:05:12 +0530

    FacebookLoginExample

---
+++ Amulya/FacebookLoginExample.java
@@ -0,0 +1,24 @@
+
+class FacebookLoginExample
+{
+public static void main(String[] args)
+{
+String firstName = ("Amulya");
+String surName = ("Bandi");
+String myFullNameis = ("Amulya Bandi"); 
+char gender = 'F';
+String emailId = ("amulya.nice@gmail.com");
+String dateOfBirth = ("28/10/94");
+long password = 123456789L;
+long phoneNumber = 7993509365l;
+System.out.println("Welcome to FacebookLogin");
+System.out.println(firstName);
+System.out.println(surName);
+System.out.println(myFullNameis);
+System.out.println(gender);
+System.out.println(emailId);
+System.out.println(dateOfBirth);
+System.out.println(password);
+System.out.println(phoneNumber);
+}
+}
