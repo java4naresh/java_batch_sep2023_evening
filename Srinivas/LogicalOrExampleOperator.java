a8343d97fb2f345df2757dda887599d7b5ddaf07
srinivasm <srinivas.madala43@gmail.com>
2023-09-16 22:33:06 +0530

    Logicaloperators

---
+++ Srinivas/LogicalOrExampleOperator.java
@@ -0,0 +1,10 @@
+class LogicalOrOperatorExample
+{
+public static void main(String [] args)
+{
+int num1 = 4;
+int num2 = 7;
+boolean finalResult = num1%2==0 || num2%2==0;
+System.out.println(finalResult);
+}
+}
---
+++ Srinivas/LogicalOroperatorExample.java
@@ -0,0 +1,10 @@
+class LogicalOroperatorExample
+{
+public static void main(String [] args)
+{
+int num1 = 2;
+int num2 = 4;
+boolean firstExample = num1/2 ==0 || num2/2==0;
+System.out.println(firstExample);
+}
+} 
