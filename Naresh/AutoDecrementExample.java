949fce200b0601dc30bff6f17ff84efdfb52e551
java4naresh <kambalanaresh4java@gmail.com>
2023-09-19 08:29:45 +0530

    if and auto increment decrement examples

--- Naresh/AutoDecrementExample.java
+++ Naresh/AutoDecrementExample.java
@@ -3,12 +3,18 @@ class AutoDecrementExample
 	public static void main(String[] args) 
 	{
 		int num = 5;
-		System.out.println(num);//5
+		/*System.out.println(num);//5
 		num--;
         System.out.println(num);//4
 		num--;
 		System.out.println(num);//3
         num--;
-		System.out.println(num);//2
+		System.out.println(num);//2*/
+		//int sum = --num + num-- + num;//4 + 4 + 3
+		//int sum = num-- + --num + --num + num-- + num;//5 + 3 + 2 + 2 + 1
+		//int sum = num++ + --num + num-- + ++num + num;//5 + 5 + 5 + 5 + 5
+		int finalSum = num-- + num++ + ++num + num + --num + num-- + ++num;//5 + 4 + 6 + 6 + 5 + 5 + 5
+		System.out.println(finalSum);//36
+		System.out.println(num);//5
 	}
 }
---
+++ Naresh/IfExample.java
@@ -0,0 +1,15 @@
+class IfExample 
+{
+	public static void main(String[] args) 
+	{
+		/*if(true) {
+		
+		System.out.println("Hello World!");
+		
+		}*/
+		int num = 11;
+		if(num%2==0) {
+		System.out.println("Given Number is Even!");
+		}
+	}
+}
