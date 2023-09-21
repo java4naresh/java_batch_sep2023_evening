d3b686acce944674598eb477a18773a1f31ccf94
srinivasm <srinivas.madala43@gmail.com>
2023-09-22 00:42:31 +0530

    IfElseProgram

---
+++ Amulya/IfElseProgram.java
@@ -0,0 +1,10 @@
+class IfElseProgram
+{
+public static void main(String[] args)
+{
+int num1 =10;
+int num2 =12;
+if(num1%2==0 && num2%2==0){System.out.println("10");}
+else if(num1%1==0 && num2%1==0){System.out.println("12");}
+}
+}
