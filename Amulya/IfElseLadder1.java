a7c5c6aae0d16138038b8827bf304fafb5f45f47
srinivasm <srinivas.madala43@gmail.com>
2023-09-22 00:48:41 +0530

    IfElseLadderAssignment

---
+++ Amulya/IfElseLadder1.java
@@ -0,0 +1,11 @@
+class IfElseLadder1
+{
+public static void main(String[] args)
+{
+int num1=12;
+int num2=8;
+int sum=num1+num2;
+if(sum==20){System.out.println(true);}
+else if(!(sum==20)){System.out.println(false);}
+}
+}
