79cfc44f756513a8692ea87e4bcb448fe94d3a7d
srinivasm <srinivas.madala43@gmail.com>
2023-09-21 13:52:05 +0530

    today practices

---
+++ Srinivas/ifElseExample1.java
@@ -0,0 +1,13 @@
+class ifElseExample1
+{
+public static void main(String[] args)
+{
+int age = 20;
+if(age <= 20)
+{
+System.out.println("Minor");
+}else{
+System.out.println("Major");
+}
+}
+}
---
+++ Srinivas/ifExample1.java
@@ -0,0 +1,45 @@
+class ifExample1
+{
+public static void main(String[] args)
+{
+int num = 10;
+if(num%2==0)
+{
+System.out.println("Given Number is Zero");
+}
+int num2 = 4;
+if(num2 >=0)
+{
+System.out.println("Given Number is Even");
+}
+int num3 = -2;
+if(num3<=0)
+{
+System.out.println("Given Number is Negative");
+}
+int age = 23;
+char gender = 'F';
+if((gender =='M' && age >=18) || (gender == 'F' && age >= 18))
+{
+System.out.println("is Eligible For Marriage");
+}
+int num4 = 9;
+int num5 = 19;
+if(++num4 == 10 && ++num5 == 20)
+{
+System.out.println("Both Are Satisfied");
+{
+System.out.println(num4);
+System.out.println(num5);
+}
+int num6 = 9;
+int num7 = 20;
+if(++num6 == 9 || ++num7 == 21)
+{
+System.out.println("Both Are Satisfied");
+}
+System.out.println(num6);
+System.out.println(num7);
+}
+}
+}
