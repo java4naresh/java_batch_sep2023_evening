d48b64bd514afcdd1c984c10a8be5fd08b368df3
srinivasm <srinivas.madala43@gmail.com>
2023-09-24 23:53:21 +0530

    ForLoopTablesExamples

---
+++ Amulya/ForLoopTable7.java
@@ -0,0 +1,11 @@
+class ForLoopTable7
+{
+public static void main(String[] args)
+{
+for (int i=1; i<11; i++)
+{
+System.out.println("7*"+i+"="+(7*i));
+}
+}
+}
+
---
+++ Amulya/ForLoopTable9.java
@@ -0,0 +1,11 @@
+class ForLoopTable9
+{
+public static void main(String[] args)
+{
+for (int i=1; i<11; i++)
+{
+System.out.println("9*"+i+"="+(9*i));
+}
+}
+}
+
