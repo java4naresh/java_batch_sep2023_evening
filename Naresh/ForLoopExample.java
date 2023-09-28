b8659167810933d68f3bef9cadc44706566ef90a
java4naresh <kambalanaresh4java@gmail.com>
2023-09-28 08:44:08 +0530

    while loop examples

--- Naresh/ForLoopExample.java
+++ Naresh/ForLoopExample.java
@@ -66,7 +66,7 @@ class ForLoopExample
 			System.out.println(i);	
 		}*/
 
-		int num = 10;//1,20(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
+		int num = 1;//1,20(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
         //boolean flag = true;
 		int divisionCount = 0;
 		for(int i=1; i<=num; i++) {//10<=10
@@ -76,7 +76,7 @@ class ForLoopExample
 		}
 		}
         
-		if(divisionCount==2) System.out.println(num+" is Prime");
+		if(divisionCount<=2) System.out.println(num+" is Prime");
 		else System.out.println(num+" is Not Prime");
 
 
---
+++ Naresh/WhileLoopExample.java
@@ -0,0 +1,52 @@
+class WhileLoopExample 
+{
+	public static void main(String[] args) 
+	{
+		/*int num = 1;
+		while(num<6) {
+		System.out.println(num);
+		num++;
+		}*/
+
+		/*int checkPrime = 10;
+		int start = 1;
+		int divisionCount = 0;
+		while(start<=checkPrime) {
+		if(checkPrime%start==0) {
+		//flag = false;
+        divisionCount++;
+		}
+		start++;
+		}
+
+		if(divisionCount<=2) System.out.println(checkPrime+" is Prime");
+		else System.out.println(checkPrime+" is Not Prime");*/
+
+        //WAP to find given number is polindrome or not
+		int num = 256;//656%10=6,656/10=65,65%10=5,65/10=6,6%10=6,6/10=0
+        int backup = num;
+		int reverse = 0;
+		while(num!=0) {//656!=0(true),65!=0(true),6!=0(true),0!=0(false)
+			int rem = num%10;//656%10=6,65%10=5,6%10=6
+			num = num/10;//656/10=65,65/10=6,6/10=0
+			reverse = (reverse*10)+rem;//(0*10)+2=,(2*10)+5=25,(25*10)+8=258 
+		}
+        System.out.println(reverse);
+		if(reverse==backup) System.out.println("Polindrome");
+		else System.out.println("Not Polindrome");
+        
+		/*int num = 256;//656%10=6,656/10=65,65%10=5,65/10=6,6%10=6,6/10=0
+        String backup = num+"";
+		String reverse = "";
+		while(num!=0) {//656!=0(true),65!=0(true),6!=0(true),0!=0(false)
+			int rem = num%10;//656%10=6,65%10=5,6%10=6
+			num = num/10;//656/10=65,65/10=6,6/10=0
+			reverse = reverse+rem;//""+6->"6","65","656"
+		}
+        System.out.println(reverse);
+		if(reverse==backup) System.out.println("Polindrome");
+		else System.out.println("Not Polindrome");*/
+
+
+	}
+}
