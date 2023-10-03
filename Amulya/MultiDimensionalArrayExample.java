d65aaf951fbf18a631c67e4224ba615e9539384f
srinivasm <srinivas.madala43@gmail.com>
2023-10-04 00:10:34 +0530

    Multi dimensional Arrays

---
+++ Amulya/MultiDimensionalArrayExample.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class MultiDimensionalArrayExample
+{
+	public static void main(String[] args) 
+	{
+		int[][] numbers = {{1, 2, 3},{4, 5, 6},{7, 8, 9 }};
+		{
+		System.out.println(numbers[0][0]);
+		System.out.println(numbers[0][1]);
+		System.out.println(numbers[0][2]);
+		System.out.println(numbers[1][0]);
+		System.out.println(numbers[1][1]);
+		System.out.println(numbers[1][2]);
+		System.out.println(numbers[2][0]);
+		System.out.println(numbers[2][1]);
+		System.out.println(numbers[2][2]);
+		}
+	}
+}
+       
+	
+
