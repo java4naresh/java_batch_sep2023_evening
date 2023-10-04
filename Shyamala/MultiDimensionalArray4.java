7b086ddcb1e35bcd52b718825cb6bbdbdeb48ad6
syamala <tellurisyamala@gmail.com>
2023-10-04 06:13:53 +0530

    Arrays,MultidiamensionalArrays Examples Practice

---
+++ Shyamala/MultiDimensionalArray4.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class MultiDimensionalArray4{
+    public static void main(String[] args) {
+        int[][] numbers = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        for (int i =numbers.length - 1; i >= 0; i--) {
+            int[] row = numbers[i];
+            for (int j = 0; j < row.length; j++) {
+                System.out.print(row[j]);
+                if (j < row.length - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Shyamala/StringDuplicateArray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+  class StringDuplicateArray{
+    public static void main(String args[]){
+	String[] arr={"Naresh","Srilatha","Syamala","Srinivas","Naresh","Amulya","Pavan","Srinivas"};
+	for(int i=0;i<=arr.length;i++){
+	for(int j=i+1;j<arr.length;j++){
+	if(arr[i].equals(arr[j])){
+	System.out.println(arr[j]);
+     }
+   }
+ }
+}
+}
