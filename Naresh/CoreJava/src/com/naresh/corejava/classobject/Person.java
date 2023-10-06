33d076f7a0aed26511f566fc1ac74d30104192eb
java4naresh <kambalanaresh4java@gmail.com>
2023-10-06 08:31:18 +0530

    object creation example

--- Naresh/CoreJava/src/com/naresh/corejava/classobject/Person.java
+++ Naresh/CoreJava/src/com/naresh/corejava/classobject/Person.java
@@ -8,5 +8,33 @@ public class Person {
 	String color;
 	double weight;
 	double height;
+	
+	public static void main(String[] args) {
+		Person naresh = new Person();
+		System.out.println(naresh);
+		naresh.name = "Naresh";
+		naresh.age = 30;
+		naresh.gender = 'M';
+		naresh.color = "white";
+		naresh.weight = 75;
+		naresh.height = 5.8;
+		System.out.println(naresh);
+		Person srinivas = new Person();
+		System.out.println(srinivas);
+		srinivas.name = "Srinivas";
+		srinivas.age = 29;
+		srinivas.gender = 'M';
+		srinivas.color = "white";
+		srinivas.weight = 75;
+		srinivas.height = 5.8;
+		System.out.println(srinivas);
+	}
+	
+	public String toString() {
+		return "Person[name="+name+", age="+age+", gender="+gender+", color="+color
+				+", weight="+weight+", height="+height+"]";
+	}
+	
+	
 
 }
