d2ebbda7ddb6df8450e0f4265799bd0360be854f
java4naresh <kambalanaresh4java@gmail.com>
2023-10-07 08:37:58 +0530

    method example

--- Naresh/CoreJava/src/com/naresh/corejava/classobject/Person.java
+++ Naresh/CoreJava/src/com/naresh/corejava/classobject/Person.java
@@ -9,25 +9,59 @@ public class Person {
 	double weight;
 	double height;
 	
+	public void awakeTime(double awakeTime) {
+		System.out.println(name+ " will wake up at "+awakeTime+" am");
+	}
+	
+	public void excersizeTime(double excersizeTime) {
+		if(age <= 30) {
+			if(excersizeTime >= 1.0) {
+				System.out.println(name+" Your Excersize spending time is correct");
+			} else {
+				System.out.println(name+" Your Excersize spending time should increase");
+			}
+		} else {
+			if(excersizeTime >= 0.45) {
+				System.out.println(name+" Your Excersize spending time is correct");
+			} else {
+				System.out.println(name+" Your Excersize spending time should increase");
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		Person naresh = new Person();
-		System.out.println(naresh);
+		//System.out.println(naresh);
 		naresh.name = "Naresh";
 		naresh.age = 30;
 		naresh.gender = 'M';
 		naresh.color = "white";
 		naresh.weight = 75;
 		naresh.height = 5.8;
-		System.out.println(naresh);
+		naresh.awakeTime(6.31);//method calling
+		naresh.excersizeTime(0.31);//method calling
+		//System.out.println(naresh);
 		Person srinivas = new Person();
-		System.out.println(srinivas);
+		//System.out.println(srinivas);
 		srinivas.name = "Srinivas";
 		srinivas.age = 29;
 		srinivas.gender = 'M';
 		srinivas.color = "white";
 		srinivas.weight = 75;
 		srinivas.height = 5.8;
-		System.out.println(srinivas);
+		srinivas.awakeTime(6.35);//method calling
+		//System.out.println(srinivas);
+		Person pavan = new Person();
+		//System.out.println(pavan);
+		pavan.name = "Pavan";
+		pavan.age = 50;
+		pavan.color = "white";
+		pavan.gender = 'M';
+		pavan.height = 5.11;
+		pavan.weight = 70.0;
+		pavan.awakeTime(6);//method calling
+		pavan.excersizeTime(0.46);//method calling
+		//System.out.println(pavan);
 	}
 	
 	public String toString() {
