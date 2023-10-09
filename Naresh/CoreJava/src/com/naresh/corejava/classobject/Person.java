bc94cb3e24f523d30029d8efe403e73e0bcbb73c
java4naresh <kambalanaresh4java@gmail.com>
2023-10-09 08:33:38 +0530

    constructor example

--- Naresh/CoreJava/src/com/naresh/corejava/classobject/Person.java
+++ Naresh/CoreJava/src/com/naresh/corejava/classobject/Person.java
@@ -9,6 +9,20 @@ public class Person {
 	double weight;
 	double height;
 	
+    public Person() {//zero param constructor
+		System.out.println("constructor");
+	}
+    
+    public Person(String name, int age, char gender, String color, 
+    		double weight, double height) {//parameter constructor
+    	this.name = name;
+    	this.age = age;
+    	this.gender = gender;
+    	this.color = color;
+    	this.weight = weight;
+    	this.height = height;
+    }
+	
 	public void awakeTime(double awakeTime) {
 		System.out.println(name+ " will wake up at "+awakeTime+" am");
 	}
@@ -30,16 +44,16 @@ public class Person {
 	}
 	
 	public static void main(String[] args) {
-		Person naresh = new Person();
-		//System.out.println(naresh);
+		Person naresh = new Person("Naresh", 30, 'M', "white", 75, 5.8);
+		System.out.println(naresh);
 		naresh.name = "Naresh";
 		naresh.age = 30;
 		naresh.gender = 'M';
 		naresh.color = "white";
 		naresh.weight = 75;
 		naresh.height = 5.8;
-		naresh.awakeTime(6.31);//method calling
-		naresh.excersizeTime(0.31);//method calling
+		//naresh.awakeTime(6.31);//method calling
+		//naresh.excersizeTime(0.31);//method calling
 		//System.out.println(naresh);
 		Person srinivas = new Person();
 		//System.out.println(srinivas);
@@ -49,7 +63,7 @@ public class Person {
 		srinivas.color = "white";
 		srinivas.weight = 75;
 		srinivas.height = 5.8;
-		srinivas.awakeTime(6.35);//method calling
+		//srinivas.awakeTime(6.35);//method calling
 		//System.out.println(srinivas);
 		Person pavan = new Person();
 		//System.out.println(pavan);
@@ -59,8 +73,8 @@ public class Person {
 		pavan.gender = 'M';
 		pavan.height = 5.11;
 		pavan.weight = 70.0;
-		pavan.awakeTime(6);//method calling
-		pavan.excersizeTime(0.46);//method calling
+		//pavan.awakeTime(6);//method calling
+		//pavan.excersizeTime(0.46);//method calling
 		//System.out.println(pavan);
 	}
 	
