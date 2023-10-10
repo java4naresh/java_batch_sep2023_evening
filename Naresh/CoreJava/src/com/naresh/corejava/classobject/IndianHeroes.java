46bb235fc023da1698118a96eccc3f2ef321d616
java4naresh <kambalanaresh4java@gmail.com>
2023-10-10 08:30:32 +0530

    constructor example

--- Naresh/CoreJava/src/com/naresh/corejava/classobject/IndianHeroes.java
+++ Naresh/CoreJava/src/com/naresh/corejava/classobject/IndianHeroes.java
@@ -4,9 +4,56 @@ public class IndianHeroes {
 	
 	String heroName;
 	int heroAge;
-	String heroColor;
-	int noOfMovies;
-	int marketCap;
-	double remunaration;
+	int heroTotalMovies;
+    int heroIndustryHits;
+
+    // this is created by me
+	/*public IndianHeroes() {
+
+	}*/
+    
+    //this is created by sts
+    public IndianHeroes() {
+    	
+    }
+    
+    //this is created by me
+	/*public IndianHeroes(String heroName, int heroAge, int heroTotalMovies, int heroIndustryHits) {
+         this.heroName = heroName;
+		 this.heroAge = heroAge;
+		 this.heroTotalMovies = heroTotalMovies;
+		 this.heroIndustryHits = heroIndustryHits;
+	}*/
+	
+    //this is created by sts
+	public IndianHeroes(String heroName, int heroAge, int heroTotalMovies, int heroIndustryHits) {
+		this.heroName = heroName;
+		this.heroAge = heroAge;
+		this.heroTotalMovies = heroTotalMovies;
+		this.heroIndustryHits = heroIndustryHits;
+	}
+	
+
+
+
+	public static void main(String[] args) 
+	{
+		IndianHeroes hero1 = new IndianHeroes();//
+        IndianHeroes hero2 = new IndianHeroes("Chiranjeevi", 67, 154, 25);
+		System.out.println(hero1);
+		System.out.println(hero2);
+	}
+
+
+	//this is created by sts
+	public String toString() {
+		return "IndianHeroes [heroName=" + heroName + ", heroAge=" + heroAge + ", heroTotalMovies=" + heroTotalMovies
+				+ ", heroIndustryHits=" + heroIndustryHits + "]";
+	}
+
+	//this is created by me
+	/*public String toString() {
+       return "IndianHeroes[heroName="+heroName+", heroAge="+heroAge+", heroTotalMovies="+heroTotalMovies+", heroIndustryHits="+heroIndustryHits+"]";
+	}*/
 
 }
