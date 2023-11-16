cdeee2b82e3009d52026b9f74c6a453eccfa84eb
java4naresh <kambalanaresh4java@gmail.com>
2023-11-16 08:32:34 +0530

    exception handling

--- Naresh/CoreJava/src/com/naresh/corejava/exceptionhandling/ExceptionExample2.java
+++ Naresh/CoreJava/src/com/naresh/corejava/exceptionhandling/ExceptionExample2.java
@@ -8,8 +8,9 @@ import java.io.IOException;
 public class ExceptionExample2 {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		ExceptionExample2.fileNotFoundExceptionExample();
+		//ExceptionExample2.fileNotFoundExceptionExample();
 		//ExceptionExample2.readDataFromFile();
+		ExceptionExample2.throwKeywordExample();
 
 	}
 	
@@ -34,6 +35,14 @@ public class ExceptionExample2 {
 		} finally {
 			
 		}
+	}
+	
+	public static void throwKeywordExample() {
+		String phonenumber = "99512887";
+		String email = "java4naresh@gmail.com";
+		if(!phonenumber.matches("[0-9]{10}") || !email.matches("[a-z0-9A-Z]+[@][a-z]+[.][a-z]+"))
+			throw new InvalidDataException("Invalid email or phone number");
+		else System.out.println("valid phone number and email");
 	}
 
 }
