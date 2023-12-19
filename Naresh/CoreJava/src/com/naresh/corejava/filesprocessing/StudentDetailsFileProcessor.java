c47b6f45765066545c5427a2cef42cbb96da7665
java4naresh <kambalanaresh4java@gmail.com>
2023-12-19 20:58:15 +0530

    student details read example

--- Naresh/CoreJava/src/com/naresh/corejava/filesprocessing/StudentDetailsFileProcessor.java
+++ Naresh/CoreJava/src/com/naresh/corejava/filesprocessing/StudentDetailsFileProcessor.java
@@ -1,9 +1,14 @@
 package com.naresh.corejava.filesprocessing;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class StudentDetailsFileProcessor {
@@ -22,6 +27,41 @@ public class StudentDetailsFileProcessor {
 		} catch(IOException ioe) {
 			return "not saved successfully";
 		} 
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public List<StudentDetails> getStudentDetails() {
+		try {
+			List<StudentDetails> studentList = new ArrayList<>();
+			File myFile = new File("D:\\files\\studentdetails.txt");
+			FileReader reader = new FileReader(myFile);
+			BufferedReader br = new BufferedReader(reader);
+			String line = br.readLine();
+			while(line != null) {
+				//System.out.println(line);
+				String[] studentStrValues = line.split("-");
+				StudentDetails details = new StudentDetails();
+				details.setStudentId(Integer.parseInt(studentStrValues[0]));
+				details.setStudentName(studentStrValues[1]);
+				details.setGender(studentStrValues[2].charAt(0));
+				details.setGrade(Integer.parseInt(studentStrValues[3]));
+				studentList.add(details);
+				line = br.readLine();
+			}
+			return studentList;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return Collections.EMPTY_LIST;
+		} 
+	}
+	
+	public static void main(String[] args) {
+		StudentDetailsFileProcessor processor = new StudentDetailsFileProcessor();
+		List<StudentDetails> studentList = processor.getStudentDetails();
+		System.out.println(studentList);
 	}
 
 }
